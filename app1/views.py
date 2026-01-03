5ab76f5ae35c1ca45a2beb884dc797a1f4e5beba
ashag909 <goudaasharani86@gmail.com>
2026-01-03 23:09:15 +0530

    edit html page

--- app1/views.py
+++ app1/views.py
@@ -22,27 +22,26 @@ def listdoctors(request):
     response=render(request,"listdoctors_temp.html",context={'qs':qs})
     return response
 
-
-
-#this ia for home page
-def home(request):
-    response=render(request,'base.html',context={})
-    return response
+def doctordelete(request,name):
+    qs=Doctor.objects.filter(name=name)
+    qs.delete()
+    res=redirect("doctorlist")
+    return res
 
 def doctoredit(request,name):
-    d=Doctor.objects.get(name=name)
+    doctor_edit=Doctor.objects.get(name=name)
     if request.method=='POST':
-        form=DoctorForm(request.post,instance=d)
+        form=DoctorForm(request.post,instance=doctor_edit) # instance means you are editing an existing object (row) instead of creating a new one.
         if form.is_valid():
             form.save()
             response=redirect('doctorlist')
             return response
-    form=DoctorForm(instance=d)
-    
-def patientlist(request):
-    qs=Patient.objects.all()
-    res=render(request,'listpatient_temp.html',context={'qs':qs})
-    return res
+    form=DoctorForm(instance=doctor_edit)
+
+#this is for home page
+def home(request):
+    response=render(request,'base.html',context={})
+    return response
 
 def patientadd(request):
     msg=""
@@ -51,6 +50,42 @@ def patientadd(request):
         if form.is_valid():
             form.save()
             msg="Patient Details Added sucssesfuly"
+    form=PatientForm()
+    res=render(request,"patientadd_temp.html",context={'form':form,'msg':msg})
+    return res
+
+def patientlist(request):
+    qs=Patient.objects.all()
+    res=render(request,'listpatient_temp.html',context={'qs':qs})
+    return res
+
+def patientedit(request,name):
+    patient_edit=Patient.objects.get(name=name)
+    if request.method=="POST":
+        form=PatientForm(request.POST,instance=patient_edit)
+        if form.is_valid():
+            form.save()
+            response=redirect("patientlist")
+            return response
+    form=PatientForm(instance=patient_edit)
+    response=redirect(request,"editpatient_temp.html",context={'form':form})
+    return response
+
+def patientdelete(request,name):
+    patient_del=Patient.objects.get(name=name)
+    patient_del.delete()
+    response=redirect('patientlist')
+    return response
 
 
 
+def appointment(request):
+    msg=""
+    if request.method =="POST":
+        d=Doctor.objects.get(name=request.POST.get('doctor'))
+        p=Patient.objects.get(name=request.POST.get('patient'))
+        date=request.POST.get('date')
+        time=request.POST.get('time')
+        desc=request.POST.get('desc')
+        Appointment.objects.create(doctor=d,patient=p,date=date,time=time,description=desc)
+        msg=""
